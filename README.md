b30e9e2c2078cb8fedfce0afa85c32afc40bcfb5
John A Stevenson <john.stevenson@ed.ac.uk>
2016-10-07 13:30:50 +0100

    Initial commit.

---
+++ README.md
@@ -0,0 +1,42 @@
+# Hekla 3 and 4 distal geochemistry
+
+I have been reconstructing the Hekla 3 and Hekla 4 eruptions based on their deposits in Iceland.  They are complicated, with multiple phases with different compositions, plume heights and dispersal directions.  Now I would like to relate these phases to their cryptotephra deposits outside Iceland.  This can be done based on their geochemistry.
+
+Below are plots of the currently available geochemical data from [Tephrabase](http://www.tephrabase.org.uk).  For Hekla 4, they show interesting features such as the narrow distribution of dacite and andesite composition material.  I can relate these to the deposits in Iceland.  For Hekla 3, there is unfortunately a real lack of data.
+
+
+## Maps of distribution of H3 and H4 by geochemistry
+
+The following maps show the results so far.
+
+![Hekla 3 rhyolite]("./Hekla 3 Tephra Rhyolite.png")
+![Hekla 3 rhyolite]("./Hekla 3 Tephra Dacite.png")
+![Hekla 3 rhyolite]("./Hekla 3 Tephra Andesite.png")
+![Hekla 4 rhyolite]("./Hekla 4 Tephra Rhyolite.png")
+![Hekla 4 rhyolite]("./Hekla 4 Tephra Dacite.png")
+![Hekla 4 rhyolite]("./Hekla 4 Tephra Andesite.png")
+
+
+## A request for data
+
+As these eruptions form such widespread tephrochronogical markers, lots more geochemical data must exist.  Much of it may only have been used to confirm that a layer was indeed H3 or H4 and nothing more.  It might be published as supplementary materials to papers, or just stored on hard drives.
+
+I would really like to see these data.
+
+The data so far are stored in the spreadsheet [hekla3and4_distal_geochemistry.csv](hekla3and4_distal_geochemistry.csv).  It has columns for _longitude, latitude, site name, major oxide analyses, total and reference_.  If you email a csv file in the same format to johnalexanderstevenson@gmail.com with your data, I will add it to the spreadsheet here and update the maps.  Of course, I will cite the referenced provided in my paper on the eruption phases.
+
+In particular, tephra glass analyses from Greenland, Faroes, Jan Mayen and the Baltic regions would be extremely valuable.
+
+
+## Plotting the figures
+
+This repository also contains the code for plotting the maps.  If you have
+Python (including the Matplotlib, Pandas and Basemap packages) and Git
+installed on your machine, you can recreate the plots as follows:
+
+```
+git clone http://github.com/volcan01010/hekla3and4_distal_geochemistry.git
+cd hekla3and4_distal_geochemistry
+python plot_hekla3and4_geochemistry_map.py
+```
+
---
+++ plot_hekla3and4_distal_geochemistry.py
@@ -0,0 +1,106 @@
+# coding: utf-8
+
+import matplotlib.pyplot as plt
+from mpl_toolkits.basemap import Basemap
+import numpy as np
+import pandas as pd
+
+
+def main():
+    """
+    Plot Hekla 3 and 4 geochemistry map
+    """
+    # Load data
+    df = pd.read_csv('hekla3and4_geochemistry_by_site.csv')
+    df['classification'] = [classify_composition(x) for x in df['SiO2']]
+
+    for tephra in ['Hekla 4 Tephra', 'Hekla 3 Tephra']:
+        for classification in ['Rhyolite', 'Dacite', 'Andesite']:
+
+            # Get coordinates
+            (lon_found, lat_found,
+             lon_absent, lat_absent) = get_coordinates(df, tephra,
+                                                       classification)
+
+            # Create figure
+            print("Plotting {} {}".format(tephra, classification))
+            plt.figure(figsize=(7, 7))
+            m = prepare_basemap()
+
+            # Plot sample locations
+            m.scatter(lon_found, lat_found, latlon=True,
+                      color='red', s=60, zorder=3,
+                      label='{}'.format(classification))
+            m.scatter(lon_absent, lat_absent, latlon=True,
+                      color='orange', s=35, zorder=2,
+                      label='Other compositions'.format(classification))
+
+            # Add decoration
+            plt.legend(loc='upper right', scatterpoints=1)
+            fig_title = '{} {}'.format(tephra, classification)
+            plt.title(fig_title)
+            plt.savefig('{}.png'.format(fig_title),
+                        bbox_inches='tight', dpi=150)
+            plt.close()
+
+
+def classify_composition(sio2):
+    """
+    Simple classification based on wt% SiO2.
+    """
+    if sio2 > 69:
+        return 'Rhyolite'
+    elif sio2 > 63:
+        return 'Dacite'
+    elif sio2 > 57:
+        return 'Andesite'
+    elif sio2 > 52:
+        return 'Basaltic andesite'
+    else:
+        return 'Basalt'
+
+
+def get_coordinates(df, tephra, classification):
+    """
+    Prepare lists of latitude and longitude of places where tephra
+    is present or absent.
+    """
+    # Locations where composition is found
+    found = ((df['tephra_name'] == tephra) &
+             (df['classification'] == classification))
+    lon_found = df.loc[found, 'longitude'].tolist()
+    lat_found = df.loc[found, 'latitude'].tolist()
+
+    # Get locations where composition is absent
+    absent = ((df['tephra_name'] == tephra) &
+              (df['classification'] != classification))
+    lon_absent = df.loc[absent, 'longitude'].tolist()
+    lat_absent = df.loc[absent, 'latitude'].tolist()
+
+    return lon_found, lat_found, lon_absent, lat_absent
+
+
+def prepare_basemap():
+    """
+    Prepare matplotlib basemap.
+    """
+    # setup lambert conformal basemap.
+    # lat_1 is first standard parallel.
+    # lat_2 is second standard parallel (defaults to lat_1).
+    # lon_0,lat_0 is central point.
+    # rsphere=(6378137.00,6356752.3142) specifies WGS84 ellipsoid
+    # area_thresh=1000 means don't plot coastline features less
+    # than 300 km^2 in area.
+    m = Basemap(width=2.8e6, height=3.2e6, rsphere=(6378137.00, 6356752.3142),
+                resolution='l', projection='lcc', area_thresh=300,
+                lat_1=45, lat_2=65, lat_0=61, lon_0=0)
+    m.drawcoastlines(linewidth=0.5, color='0.4')
+    m.fillcontinents(color=[0.9, 0.9, 0.9], zorder=0)
+    m.drawparallels(np.arange(-30, 81, 10.), labels=[1, 1, 0, 0])
+    m.drawmeridians(np.arange(-50, 51, 10.), labels=[0, 0, 0, 1])
+    
+    return m
+
+
+if __name__ == '__main__':
+    main()
